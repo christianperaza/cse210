cce8b588deeb138940c7ef32088426012f54c59a
IamFisherman <christianperaza2000@gmail.com>
2023-11-07 10:02:41 -0400

    Create a List to iterate shapes and works well

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -9,14 +9,19 @@ class Program
         Rectangle rectangle = new Rectangle("red", 5, 5);
         Circle circle = new Circle("yellow", 2);
 
-        Console.WriteLine(square.GetColor());
-        Console.WriteLine(square.GetArea());
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
 
-        Console.WriteLine(rectangle.GetColor());
-        Console.WriteLine(rectangle.GetArea());
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
 
-        Console.WriteLine(circle.GetColor());
-        Console.WriteLine(circle.GetArea());
+            Console.WriteLine(color);
+            Console.WriteLine(area);
+        }
 
     }
 }
