343820dcaae904455df0317fc1c9171af28e138e
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 08:03:38 -0400

    Comment class comments added

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,8 +1,10 @@
 public class Comment
 {
+    // attributes...
     public string _personName;
     public string _comment;
 
+    // method...
     public void DisplayComment()
     {
         Console.WriteLine($"@{_personName}:");
