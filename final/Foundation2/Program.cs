e14c16957399fe495663ca4893bff67ae49c470d
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 13:02:57 -0400

    2° product added

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,18 +5,20 @@ class Program
     static void Main(string[] args)
     {
 
-        // ----------- 1° ORDER -----------
+        // ----------- 1° ORDER ----------- //
 
+        // product 1...
         string nameProduct1 = "Shampoo Sweet";
         int idProduct1 = 56841236;
         double priceProduct1 = 5.3;
         int quantityProduct1 = 2;
         Product product1 = new Product(nameProduct1, idProduct1, priceProduct1, quantityProduct1);
-
-        string nameProduct2 = "Shampoo Sweet";
-        int idProduct2 = 56841236;
-        double priceProduct2 = 5.3;
-        int quantityProduct2 = 2;
+        
+        // product 2...
+        string nameProduct2 = "Ruby Soap";
+        int idProduct2 = 45789122;
+        double priceProduct2 = 3;
+        int quantityProduct2 = 5;
         Product product2 = new Product(nameProduct2, idProduct2, priceProduct2, quantityProduct2);
 
         Order order1 = new Order();
