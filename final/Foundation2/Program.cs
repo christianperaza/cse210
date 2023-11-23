85bf8ced075c9628ad57823ca9c860693f12cb66
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 19:43:48 -0400

    Order 2 products created

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -39,18 +39,25 @@ class Program
         // ----------- 2° ORDER ----------- //
 
         // product 1 of order 2...
-        string nameProduct1Order2 = "Shampoo Sweet";
-        int idProduct1Order2 = 56841236;
-        float priceProduct1Order2 = 5.3f;
-        int quantityProduct1Order2 = 2;
+        string nameProduct1Order2 = "Sunrise Wheat Flour";
+        int idProduct1Order2 = 12485556;
+        float priceProduct1Order2 = 4.2f;
+        int quantityProduct1Order2 = 4;
         Product product1Order2 = new Product(nameProduct1Order2, idProduct1Order2, priceProduct1Order2, quantityProduct1Order2);
 
         // product 2 of order 2...
-        string nameProduct2Order2 = "Ruby Soap";
-        int idProduct2Order2 = 45789122;
-        float priceProduct2Order2 = 3f;
-        int quantityProduct2Order2 = 5;
+        string nameProduct2Order2 = "Carton of Eggs";
+        int idProduct2Order2 = 22589431;
+        float priceProduct2Order2 = 8.5f;
+        int quantityProduct2Order2 = 1;
         Product product2Order2 = new Product(nameProduct2Order2, idProduct2Order2, priceProduct2Order2, quantityProduct2Order2);
+
+        // product 3 of order 2...
+        string nameProduct3Order2 = "Liter of Milk";
+        int idProduct3Order2 = 11147852;
+        float priceProduct3Order2 = 6f;
+        int quantityProduct3Order2 = 2;
+        Product product3Order2 = new Product(nameProduct3Order2, idProduct3Order2, priceProduct3Order2, quantityProduct3Order2);
 
         // customer 2...
         string nameCustomer2 = "Pablo Perez";
@@ -64,6 +71,7 @@ class Program
         Order order2 = new Order(nameCustomer2, street2, city2, state2, country2);
         order2.SetProductInList(product1Order2);
         order2.SetProductInList(product2Order2);
+        order2.SetProductInList(product3Order2);
 
         
 
