4949923afd3e932239535f47188e8171d2ddb2b6
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 08:09:59 -0400

    Program class comments improved

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // video 1...
-        Video video1 = new Video();
+        // --------- video 1 --------- //
+
+        // create and instance of a new Video object...
+        Video video1 = new();
         video1._title = "Lewis Capaldi - Before You Go (Official Video)";
         video1._author = "Lewis Capaldi";
         video1._lengthInSeconds = 247;
+
         // comments of video 1...
         Comment comment1 = new Comment(); // 1
         comment1._personName = "jackie";
@@ -19,13 +22,21 @@ class Program
         Comment comment3 = new Comment(); // 3
         comment3._personName = "pablo-19";
         comment3._comment = "¡Qué delicia de canción! Ya la he reproducido un par de veces y no me canso.";
+
         // now, we add comments to list of comments in video1...
         video1._listOfComments.Add(comment1);    
         video1._listOfComments.Add(comment2);
         video1._listOfComments.Add(comment3);
         video1._numberOfComments = video1._listOfComments.Count; // set number of comments counting the comments in list
+
         // display video 1...
         video1.DisplayVideoInfo();
+
+
+        
+
+
+        
         
     }
 }
