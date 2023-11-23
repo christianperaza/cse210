30a39154305cef4db8276b9dd9f8ff59a749a11b
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 08:02:20 -0400

    Video class comments added

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,21 +1,25 @@
 public class Video
 {
+    // attributes...
     public string _title;
     public string _author;
     public int _lengthInSeconds;
     public int _numberOfComments;
     public List<Comment> _listOfComments = new List<Comment>();
 
+    // method...
     public void DisplayVideoInfo()
     {
-        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("-----------------------------------------"); // design
         Console.WriteLine($"{_title}");
         Console.WriteLine($"by {_author}");
         Console.WriteLine($"Duration: {_lengthInSeconds} seconds\n");
 
         Console.WriteLine($"{_numberOfComments} comments:\n");
+
         for (int i = 0; i < _listOfComments.Count; i++)
         {
+            // these if conditions are just to avoid that last comment display a new white space after it
             if (i + 1 < _listOfComments.Count)
             {
                 _listOfComments[i].DisplayComment();
@@ -26,6 +30,7 @@ public class Video
                 _listOfComments[i].DisplayComment();
             }
         }
-        Console.WriteLine("-----------------------------------------");
+
+        Console.WriteLine("-----------------------------------------"); // design
     }
 }
