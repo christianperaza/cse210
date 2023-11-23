51296e42e55bac108e789a217e7dccc6ef011c81
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 13:44:26 -0400

    DisplayAddress method created

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -4,4 +4,9 @@ public class Address
     private string _city;
     private string _stateProvince;
     private string _country;
+
+    public string DisplayAddress()
+    {
+        return $"{_street} \n {_city} \n {_stateProvince}, {_country}";
+    }
 }
