8192aa162febf42e3bb23c11669819027f84824d
IamFisherman <christianperaza2000@gmail.com>
2023-11-23 07:25:38 -0400

    Comments added to listOfComments: it didn't work

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -14,7 +14,16 @@ class Program
         Comment comment1 = new Comment();
         comment1._personName = "jackie";
         comment1._comment = "I love so much this song!";
-        comment1.DisplayComment();
+        Comment comment2 = new Comment();
+        comment2._personName = "PuffingTom";
+        comment2._comment = "I can remember my mom with this song. It's so beautiful.";
+        Comment comment3 = new Comment();
+        comment3._personName = "pablo-19";
+        comment3._comment = "¡Qué delicia de canción! Ya la he reproducido un par de veces y no me canso.";
+
+        video1._listOfComments.Add(comment1);    
+        video1._listOfComments.Add(comment2);
+        video1._listOfComments.Add(comment3);
         
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,13 +3,19 @@ public class Video
     public string _title;
     public string _author;
     public int _lengthInSeconds;
-    //public int _numberOfComments;
-    //public List<Comment> _listOfComments = new List<Comment>();
+    // public int _numberOfComments;
+    public List<Comment> _listOfComments = new List<Comment>();
 
     public void DisplayVideoInfo()
     {
         Console.WriteLine($"{_title}");
         Console.WriteLine($"by {_author}");
-        Console.WriteLine($"Duration: {_lengthInSeconds} seconds");
+        Console.WriteLine($"Duration: {_lengthInSeconds} seconds\n");
+
+        Console.WriteLine("Comments:");
+        for (int i = 0; i < _listOfComments.Count; i++)
+        {
+            _listOfComments[i].DisplayComment();
+        }
     }
 }
